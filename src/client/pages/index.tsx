1d545f08ea7dff8073e2ff07ff2e0b38c2f827b9
aajungroth <aajungroth@ucdavis.edu>
2021-05-12 11:06:19 -0700

    Add index.tsx

---
+++ src/client/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <h1>Hello, World!</h1>
+      <h2>Testing</h2>
+    </>
+  )
+}
+
+export default Home;
